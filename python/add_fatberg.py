881ccf5062bf499376b24ce2f1254af978d75c02
Matthew Shen <matthewdshen@gmail.com>
2022-11-17 15:59:24 -0500

    attempt test with new .inp

--- python/add_fatberg.py
+++ python/add_fatberg.py
@@ -1,48 +1,37 @@
 import pyswmm
 import swmm_api
+import random
 
-
-
-'''
-Summary: The following function runs the input file and changes a pipe from circular to filled circular
-Inputs: path of input file
-Output: report file
-'''
 
 
 
 str_path = '/home/matthewdshen/GitHub/Fatberg_Detector/test/Test_Network_clear.inp'
 
-inp = swmm_api.SwmmInput.read_file(str_path)
+def func_add_fatberg(str_path):
+    
+    '''
+    Summary: The following function runs the input file and changes a pipe from circular to filled circular
+    Inputs: path of input file
+    Output: report file
+    '''
+    with pyswmm.Simulation(str_path) as sim:
+        lst_linkids = []
 
-inp[swmm_api.input_file.section_labels.XSECTIONS]['1'].shape = 'FILLED_CIRCULAR'
+        for link in pyswmm.Links(sim):
+            lst_linkids.append(link.linkid)
 
-inp[swmm_api.input_file.section_labels.XSECTIONS]['1'].transect = 0.75
+        inp = swmm_api.SwmmInput.read_file(str_path)
 
-inp.write_file('new_inputfile.inp')
+        fatberg_link = random.choice(lst_linkids)
 
+        # inp[swmm_api.input_file.section_labels.XSECTIONS][fatberg_link].shape = 'FILLED_CIRCULAR'
 
+        inp[swmm_api.input_file.section_labels.XSECTIONS][fatberg_link].transect = .75
 
-
-
-
-
-
-
-
-
-
-
-# with pyswmm.Simulation(str_path) as sim:
-#     lst_linkids = []
-
-#     for link in pyswmm.Links(sim):
-#         lst_linkids.append(link.linkid)
-
-#     print(lst_linkids)
+        inp.write_file('/home/matthewdshen/GitHub/Fatberg_Detector/test/Test_Network_fatberg.inp')
 
 
-
+func_add_fatberg(str_path)
 
 
 
---
+++ python/nodes_total_inflow.py
@@ -0,0 +1,75 @@
+import pyswmm
+import pandas as pd
+import matplotlib.pyplot as plt
+from swmm.toolkit.shared_enum import NodeAttribute
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+input_file = '/home/matthewdshen/GitHub/Fatberg_Detector/test/Test_Network_fatberg.inp'
+
+def get_inflow(input_file,n_node):
+  ''' function to get the total inflow from one node at all steps in the simulation and save it in a dataframe '''
+
+  # create an Empty DataFrame object
+  df_node = pd.DataFrame()
+  simulation_time = []
+  node_ids = []
+  total_inflow = []
+  
+  with pyswmm.Simulation(inputfile=input_file) as sim:
+
+    # Initialize nodes and links objects
+    nodes = pyswmm.Nodes(sim)
+
+    # Specify the link or nodes that we are interested in monitoring
+    node = nodes[n_node]
+  
+    # itterate through each step in simulation
+    for step in sim:
+
+        # Log the simulation time
+        simulation_time.append(sim.current_time)
+
+        # Log node id and total_inflow
+        #node_ids.append(node.nodeid)
+        total_inflow.append(node.total_inflow)
+
+    # create dataframe with step data for that node and update column name
+    df_node = pd.DataFrame(total_inflow, simulation_time)
+    df_node.columns =[n_node]
+
+    return df_node
+
+# combine all nodes into one dataframe
+def combine_allnodes(input_file, node_num):
+  ''' function to combine inflow and time step data from all nodes from input file into one dataframe 
+      Inputs are simulation file path and list of node numbers'''
+
+  # initialize dataframe
+  df_allflow = pd.DataFrame()
+
+  # itterate through each node
+  for s in node_num:
+
+    # run get_inflow function on current node and add it to that column in dataframe
+    df_allflow[s]=(get_inflow(input_file, s))
+
+  return df_allflow
+
+# list of node names
+node_list = ['J1','J2','J3','J4','J5','J6','J7','J8','J9','J10','J11','J12']
+
+# create dataframe with simulation data for all nodes
+allflow=combine_allnodes(input_file, node_list)
+allflow
+
+plt.figure(figsize=(20, 5), dpi=100)
+
+for n in node_list:
+  plt.plot(allflow[n], linewidth=2.0, label = n)
+
+plt.title("flows at each node")
+plt.ylabel("Flow (CFS)")
+plt.xlabel("Simulation Time")
+plt.legend(loc="upper right")
+plt.show()
