1b0054ca3c7b3ed681dd41c56ead5eb6bcf47189
MatthewDShen <matthewdshen@gmail.com>
2022-12-24 21:36:36 -0500

    clean up input and output files

--- python/fast_find_fatberg.py
+++ python/fast_find_fatberg.py
@@ -3,44 +3,60 @@ import pyswmm
 import random
 import swmm.toolkit
 import sys
+from collections import subtract
 
 # Clean file path inp
 str_inp_clean = '/home/matthewdshen/GitHub/fatberg_detector/swmm_files/clean/Test_Network_clean.inp'
 
 # Dirty path file inp
-str_inp_dirty = '/home/matthewdshen/GitHub/fatberg_detector/swmm_files/dirty/Test_Network_dirty1.inp'
+str_inp_dirty = '/home/matthewdshen/GitHub/fatberg_detector/swmm_files/dirty/Test_Network_dirty'+str(random.randint(1,22))+'.inp'
+
+# List of nodes with sensors
+lst_sensor_nodes = ['J1','J2','J3','J4','J5','J6','J7','J8','J9']
+
 
 # Set up clean simulation
-sim_clean = pyswmm.Simulation(str_inp_clean)
-# Run clean simulation
-for steps in sim_clean:
-        pass
+with pyswmm.Simulation(str_inp_clean) as sim_clean:
+    # Run clean simulation
+    for steps in sim_clean:
+            pass
 
-# Set up dirty simulation
-sim_dirty = pyswmm.Simulation(str_inp_dirty)
-# Run dirty simulation
-for steps in sim_dirty:
-        pass
+    # Get nodes from simulation
+    lst_nodes_clean = pyswmm.nodes.Nodes(sim_clean)
 
-# Get nodes from simulation
-lst_nodes = pyswmm.nodes.Nodes(sim_clean)
+    # # Pull links from simulation
+    # lst_links_clean = pyswmm.Links(sim_clean)
 
-# Pull links from simulation
-lst_links = pyswmm.Links(sim_clean)
+    # Save cumulative inflow at each node
+    dic_clean_flows = {}
+    for node in lst_nodes_clean:
+        dic_clean_flows[node.nodeid] = node.cumulative_inflow
+        
+
+
+
+with pyswmm.Simulation(str_inp_dirty) as sim_dirty:
+
+    # Run dirty simulation
+    for steps in sim_dirty:
+            pass
     
-# List of nodes with sensors
-lst_sensor_nodes = ['J1','J9']
+    # Pull dirty nodes from simulation
+    lst_nodes_dirty = pyswmm.nodes.Nodes(sim_dirty)
 
-# Loop through each 
+    # Pull dirty links from simulation
+    lst_links_dirty = pyswmm.links.Links(sim_dirty)
 
-print(lst_nodes['J1'].cumulative_inflow)
+    # Save cumulative inflow at each node
+    dic_dirty_flows = {}
+    for node in lst_nodes_dirty:
+        dic_dirty_flows[node.nodeid] = node.cumulative_inflow
 
-
-
-
-
-
-
+    # Get change in flow rate from clean and dirty network
+    # print(subtract(dic_clean_flows) - subtract(dic_dirty_flows))
+    # for link in lst_links_dirty:
+    #     print(link.connections[0])
+    #     print()
 
 
 
